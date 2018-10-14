omnirom/android_packages_services_Telephony

CallFeatureSettings: Only show video quality if QTI package installed
Shamu uses org.codeaurora.ims package and different constants for video
quality settings, so this crashes on open.

Change-Id: I2257987f7166b24473140c86255d2109e7117b24

src/com/android/phone/CallFeaturesSetting.java
@@ -59,6 +59,8 @@ import com.android.phone.settings.VoicemailSettingsActivity;
 import com.android.phone.settings.fdn.FdnSetting;
 import com.android.services.telephony.sip.SipUtil;
 
+import com.android.internal.util.omni.PackageUtils;
+
 import java.lang.String;
 import java.util.ArrayList;
 import java.util.List;
@@ -353,6 +355,10 @@ public class CallFeaturesSetting extends PreferenceActivity
                         CarrierConfigManager.KEY_CARRIER_VOLTE_TTY_SUPPORTED_BOOL)) {
             TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
             /* tm.listen(mPhoneStateListener, PhoneStateListener.LISTEN_CALL_STATE); */
+        }
+
+        if (!PackageUtils.isAppInstalled(this, "com.qualcomm.qti.ims")) {
+            prefSet.removePreference(findPreference("ims_settings_key"));
         }
 
         Preference wifiCallingSettings = findPreference(

Project-Xtended/packages_services_Telephony

Signed-off-by: DennySPB <dennyspb@gmail.com>

src/com/android/phone/CallFeaturesSetting.java
@@ -53,6 +53,8 @@ import com.android.phone.settings.PhoneAccountSettingsFragment;
 import com.android.phone.settings.VoicemailSettingsActivity;
 import com.android.phone.settings.fdn.FdnSetting;
 
+import com.android.internal.util.xtended.XtendedUtils;
+
 import java.util.List;
 
 import org.codeaurora.ims.QtiCallConstants;
@@ -408,6 +410,10 @@ public class CallFeaturesSetting extends PreferenceActivity
                         CarrierConfigManager.KEY_CARRIER_VOLTE_TTY_SUPPORTED_BOOL)) {
             TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
             /* tm.listen(mPhoneStateListener, PhoneStateListener.LISTEN_CALL_STATE); */
+        }
+
+        if (!XtendedUtils.isPackageInstalled(this, "com.qualcomm.qti.ims")) {
+            prefSet.removePreference(findPreference("ims_settings_key"));
         }
 
         final PhoneAccountHandle simCallManager = mTelecomManager.getSimCallManager();